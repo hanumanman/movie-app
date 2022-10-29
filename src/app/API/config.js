96853580c3ed5f84129fbf08fe91b295909c9dcd
Itztla <hanumanman37@gmail.com>
2022-10-29 21:13:31 +0700

    commmit

---
+++ src/app/API/config.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "c3d0338c685314581c50db43fa93a914";
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../components/Header/navbar/NavBar";
 import MovieList from "../../components/MovieList/MovieList";
 import Preview from "../../components/Preview/Preview";
+import { API_KEY } from "../../app/API/config";
+import apiService from "../../app/API/APIService";
 
 function MainPage() {
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const res = await apiService.get(
+          `discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMovies();
+  }, []);
+
   return (
     <div>
       <NavBar />
